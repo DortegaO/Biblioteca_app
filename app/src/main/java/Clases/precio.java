f888464f1a30b0ca67e0f363e56890dc1e65a925
DortegaO <danielortega.o@gmail.com>
2020-11-11 21:51:26 -0300

    Initial commit

---
+++ app/src/main/java/Clases/precio.java
@@ -0,0 +1,35 @@
+package Clases;
+
+public class precio
+{
+    private String Farenheith;
+    private String Revival;
+    private String Alquimista;
+
+
+    public precio()
+    {
+        Farenheith = "5000";
+        Revival = "12000";
+        Alquimista = "45000";
+
+    }
+
+    public String getFarenheith()
+    {
+        return Farenheith;
+    }
+
+    public String getRevival()
+    {
+        return Revival;
+    }
+
+    public String getAlquimista()
+    {
+        return Alquimista;
+    }
+
+
+
+}
